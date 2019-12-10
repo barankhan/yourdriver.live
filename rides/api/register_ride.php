956a8310934ae5bbb3e06771f86fa2db3dd0d420
Baran Khan <selfcoordinator@gmail.com>
2019-12-10 17:03:26 +0500

    Driver Bulain step 1

--- rides/api/register_ride.php
+++ rides/api/register_ride.php
@@ -4,12 +4,12 @@ require_once __DIR__."/../../model/user.php";
 require_once __DIR__."/../../model/ride.php";
 require_once __DIR__."/../../model/rideAlert.php";
 require_once __DIR__."/../../utils/firebaseNotification.php";
-
+$mobile= $_REQUEST['mobile'];
 $lr = new LogRequest();
 $lr->setRequestUri($_SERVER['REQUEST_URI']);
 $lr->setRequestBody(json_encode($_REQUEST));
 $lr->setRequestHeader(json_encode($_SERVER));
-$lr->setMobileNumber($_REQUEST['mobile']);
+$lr->setMobileNumber($mobile);
 $lr->insertLog();
 $userObj = new User();
 $userObj->getUserWithMobile($mobile);
