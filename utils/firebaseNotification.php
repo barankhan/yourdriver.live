eba624331e27b6a690b37b76ab6cccc8c7d1a9a7
Baran Khan <selfcoordinator@gmail.com>
2019-12-17 16:13:07 +0500

    firebase libraray update

--- utils/firebaseNotification.php
+++ utils/firebaseNotification.php
@@ -1,5 +1,10 @@
 <?php
-require_once __DIR__."/../model/FirebaseLog.php";
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Messaging\Notification;
+use Kreait\Firebase\Messaging\AndroidConfig;
 
 /**
  * Created by PhpStorm.
@@ -9,49 +14,115 @@ require_once __DIR__."/../model/FirebaseLog.php";
  */
 class firebaseNotification
 {
-
-     private $firebaseAPIKey ="AIzaSyDTzHTWNjCuve-ynDUDpZXrv8TCIbwzTtc";
-     private $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
-
-     public function sendPayloadOnly($logId,$token,$payload=null,$notification=null,$priority='normal'){
-         $fbaseLogObj  = new FirebaseLog();
-         $fbaseLogObj->setNotification(json_encode($notification));
-         $fbaseLogObj->setPayload(json_encode($payload));
-         $fbaseLogObj->setRequestLogId($logId);
-         $fbaseLogObj->setFirebaseKey($token);
+    public function sendPayLoadOnly($logId, $token, $payload = null, $notification = null, $priority = 'normal')
+    {
+        $fbaseLogObj = new FirebaseLog();
+        $fbaseLogObj->setNotification(json_encode($notification));
+        $fbaseLogObj->setPayload(json_encode($payload));
+        $fbaseLogObj->setRequestLogId($logId);
+        $fbaseLogObj->setFirebaseKey($token);
 
 
+        $factory = (new Factory())
+            ->withServiceAccount(__DIR__ . '/firebaskey.json');
+//        $notification = Notification::fromArray($notification);
+
+
+        $config = AndroidConfig::fromArray([
+            'ttl' => '10s',
+            'priority' => $priority,
+            'notification' => $notification,
+            'data'=>$payload
+        ]);
+
+
+        $messaging = $factory->createMessaging();
+        $message = CloudMessage::withTarget('token', $token);
+//        $message = $message->withData($payload);
+        $message = $message->withAndroidConfig($config);
 
 
 
-         $fcmNotification['to'] =  $token;
-         $fcmNotification['priority'] =  $priority;
-         if(!empty($notification)){
-             $fcmNotification['notification'] = $notification;
-         }
-         if(!empty($payload)){
-             $fcmNotification['data'] = $payload;
-         }
-         $headers = [
-             'Authorization: key=' . $this->firebaseAPIKey,
-             'Content-Type: application/json'
-         ];
-         $ch = curl_init();
-         curl_setopt($ch, CURLOPT_URL,$this->fcmUrl);
-         curl_setopt($ch, CURLOPT_POST, true);
-         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
-         $result = curl_exec($ch);
-         curl_close($ch);
+//        $message->withNotification($notification);
 
-         $res_obj = json_decode($result);
-         $fbaseLogObj->setFirebaseResponse($result);
-         $fbaseLogObj->setFirebaseMessageId($res_obj->results[0]->message_id);
-         $fbaseLogObj->insert();
+//        if (!empty($notification)) {
+//            $message;
+//        }
+//        if (!empty($payload)) {
+//            $message->withData($payload);
+//        }
 
-         return $result;
+
+//        try {
+//            $messaging->validate($message);
+//        } catch (InvalidMessage $e) {
+//            print_r($e->errors());
+//        }
+//
+//        die;
+
+        $result = $messaging->send($message);
+        $res = explode("/", $result["name"]);
+        $fbaseLogObj->setFirebaseResponse(json_encode($result));
+        $fbaseLogObj->setFirebaseMessageId($res[3]);
+        $fbaseLogObj->insert();
+
+
+
+        return $result;
     }
 
+
 }
+
+
+
+
+//class firebaseNotification
+//{
+//
+//     private $firebaseAPIKey ="AIzaSyDTzHTWNjCuve-ynDUDpZXrv8TCIbwzTtc";
+//     private $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
+//
+//     public function sendPayloadOnly($logId,$token,$payload=null,$notification=null,$priority='normal'){
+//         $fbaseLogObj  = new FirebaseLog();
+//         $fbaseLogObj->setNotification(json_encode($notification));
+//         $fbaseLogObj->setPayload(json_encode($payload));
+//         $fbaseLogObj->setRequestLogId($logId);
+//         $fbaseLogObj->setFirebaseKey($token);
+//
+//
+//
+//
+//
+//         $fcmNotification['to'] =  $token;
+//         $fcmNotification['priority'] =  $priority;
+//         if(!empty($notification)){
+//             $fcmNotification['notification'] = $notification;
+//         }
+//         if(!empty($payload)){
+//             $fcmNotification['data'] = $payload;
+//         }
+//         $headers = [
+//             'Authorization: key=' . $this->firebaseAPIKey,
+//             'Content-Type: application/json'
+//         ];
+//         $ch = curl_init();
+//         curl_setopt($ch, CURLOPT_URL,$this->fcmUrl);
+//         curl_setopt($ch, CURLOPT_POST, true);
+//         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+//         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fcmNotification));
+//         $result = curl_exec($ch);
+//         curl_close($ch);
+//
+//         $res_obj = json_decode($result);
+//         $fbaseLogObj->setFirebaseResponse($result);
+//         $fbaseLogObj->setFirebaseMessageId($res_obj->results[0]->message_id);
+//         $fbaseLogObj->insert();
+//
+//         return $result;
+//    }
+//
+//}
