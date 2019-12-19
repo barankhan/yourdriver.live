3fabea9f26e228f6b37132afab7004171b543244
Baran Khan <selfcoordinator@gmail.com>
2019-12-19 15:49:38 +0500

    ride started ended arrived

--- model/ride.php
+++ model/ride.php
@@ -12,7 +12,7 @@ class ride extends  baseModel implements JsonSerializable
 
     private $id=0,$passengerId,$driverId=0,$createdAt,$updatedAt,$pickupLat,$pickupLng,$vehicleType,$dropoffLat
     ,$dropoffLng,$response,$message,$isRideStarted=0,$isRideCancelled=0,$rideStartedAt,$rideCancelledAt,$driverLat,$driverLng,
-        $cancelledByTypeId=0,$isDriverArrived=0;
+        $cancelledByTypeId=0,$isDriverArrived=0,$isRideEnded=0,$rideEndedAt,$driverArrivedAt;
     public function insert(){
         $q = "insert into rides(passenger_id,pickup_lat,pickup_lng,vehicle_type,dropoff_lat,dropoff_lng)
             values(:passenger_id,:pickup_lat,:pickup_lng,:vehicle_type,:dropoff_lat,:dropoff_lng);";
@@ -25,11 +25,12 @@ class ride extends  baseModel implements JsonSerializable
     public function update(){
         $q = "update rides set is_ride_started=:isRideStarted,is_ride_cancelled=:isRideCancelled,ride_started_at=:rideStartedAt,
 ride_cancelled_at=:rideCancelledAt,driver_lat=:driverLat,driver_lng=:driverLng,cancelled_by_type_id=:cancelledByTypeId,
-is_driver_arrived=:isDriverArrived where id=:id";
+is_driver_arrived=:isDriverArrived,driver_arrived_at=:driverArrivedAt,is_ride_ended=:isRideEnded,ride_ended_at=:rideEndedAt where id=:id";
         $params = array("isRideStarted"=>$this->isRideStarted,"isRideCancelled"=>$this->isRideCancelled,
             "rideStartedAt"=>$this->rideStartedAt,"rideCancelledAt"=>$this->rideCancelledAt,"id"=>$this->id,
             "driverLat"=>$this->driverLat,"driverLng"=>$this->driverLng,"cancelledByTypeId"=>$this->cancelledByTypeId,
-            "isDriverArrived"=>$this->isDriverArrived
+            "isDriverArrived"=>$this->isDriverArrived,"driverArrivedAt"=>$this->driverArrivedAt,"isRideEnded"=>$this->isRideEnded,
+            "rideEndedAt"=>$this->rideEndedAt
         );
 
         return $this->executeUpdate($q,$params);
@@ -86,6 +87,56 @@ is_driver_arrived=:isDriverArrived where id=:id";
         $params= array("id"=>$this->id);
         $this->setAllFields($this->executeSelectSingle($q,$params));
     }
+
+    /**
+     * @return int
+     */
+    public function getIsRideEnded()
+    {
+        return $this->isRideEnded;
+    }
+
+    /**
+     * @param int $isRideEnded
+     */
+    public function setIsRideEnded($isRideEnded)
+    {
+        $this->isRideEnded = $isRideEnded;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRideEndedAt()
+    {
+        return $this->rideEndedAt;
+    }
+
+    /**
+     * @param mixed $rideEndedAt
+     */
+    public function setRideEndedAt($rideEndedAt)
+    {
+        $this->rideEndedAt = $rideEndedAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDriverArrivedAt()
+    {
+        return $this->driverArrivedAt;
+    }
+
+    /**
+     * @param mixed $driverArrivedAt
+     */
+    public function setDriverArrivedAt($driverArrivedAt)
+    {
+        $this->driverArrivedAt = $driverArrivedAt;
+    }
+
+
 
     /**
      * @return mixed
