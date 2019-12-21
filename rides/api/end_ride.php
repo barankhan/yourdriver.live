8a16a2f1265cb9ebd6530b1e3cd1e8990980bb66
Baran Khan <selfcoordinator@gmail.com>
2019-12-21 09:06:31 +0500

    end ride with transaction

--- rides/api/end_ride.php
+++ rides/api/end_ride.php
@@ -13,32 +13,38 @@ $driverObj->getUserWithMobile($mobile);
 $rideObj = new ride();
 $rideObj->setId($_REQUEST['ride_id']);
 $rideObj->findRideWithId();
-$rideObj->setIsRideEnded(1);
-$rideObj->setRideEndedAt(date("Y-m-d H:i:s"));
-$rideObj->setDistance($_REQUEST['distance']);
-$rideObj->update();
+$transObj = new DriverTransaction();
 
-$basePrice = basePrice::getBasePrice($rideObj->getVehicleType(),$rideObj->getPickupLat(),$rideObj->getPickupLng());
-$transObj = Misc::generateCompletedRideTransaction($rideObj,$basePrice);
-$driverObj->setBalance($driverObj->getBalance()-$transObj->getCompanyServiceCharges());
-$driverObj->setIsDriverOnTrip(0);
-$driverObj->update();
-$transObj->setDriverBalance($driverObj->getBalance());
-$rideObj->setResponse("ride_Ended");
-$rideObj->setMessage("Thanks for the ride! We hope you have enjoyed the service.");
-$passengerObj  = new User();
-$passengerObj->getUserWithId($rideObj->getPassengerId());
-$notification['title']='Thanks Dude';
-$notification['body']='Thanks for the ride! We hope you have enjoyed the service.Fare is:'.$transObj->getTotalFare();
-$payload['do']="ride_ended";
-$payload['msg']="Thanks for the ride! We hope you have enjoyed the service.";
-$payload['key']="ride_ended";
+if($rideObj->getIsRideEnded()==0) {
+    $rideObj->setIsRideEnded(1);
+    $rideObj->setRideEndedAt(date("Y-m-d H:i:s"));
+    $rideObj->setDistance($_REQUEST['distance']);
+    $rideObj->update();
 
-$fbaseObj = new firebaseNotification();
-
-
-$token = $passengerObj->getFirebaseToken();
-$fabseRes = $fbaseObj->sendPayloadOnly($lr->getId(),$token,$payload,$notification,'normal');
+    $basePrice = basePrice::getBasePrice($rideObj->getVehicleType(), $rideObj->getPickupLat(), $rideObj->getPickupLng());
+    $transObj = Misc::generateCompletedRideTransaction($rideObj, $basePrice);
+    $driverObj->setBalance($driverObj->getBalance() - $transObj->getCompanyServiceCharges());
+    $driverObj->setIsDriverOnTrip(0);
+    $driverObj->update();
+    $transObj->setResponse("ride_ended_successfully");
+    $transObj->setMessage("Your ride has been ended successfully");
+    $transObj->setDriverBalance($driverObj->getBalance());
+    $rideObj->setResponse("ride_Ended");
+    $rideObj->setMessage("Thanks for the ride! We hope you have enjoyed the service.");
+    $passengerObj = new User();
+    $passengerObj->getUserWithId($rideObj->getPassengerId());
+    $notification['title'] = 'Thanks Dude';
+    $notification['body'] = 'Thanks for the ride! We hope you have enjoyed the service.Fare is:' . $transObj->getTotalFare();
+    $payload['do'] = "ride_ended";
+    $payload['msg'] = "Thanks for the ride! We hope you have enjoyed the service.";
+    $payload['key'] = "ride_ended";
+    $fbaseObj = new firebaseNotification();
+    $token = $passengerObj->getFirebaseToken();
+    $fabseRes = $fbaseObj->sendPayloadOnly($lr->getId(), $token, $payload, $notification, 'normal');
+}else{
+    $transObj->setResponse("ride_ended_error");
+    $transObj->setMessage("Ride has already been ended.!");
+}
 
 
 $var = json_encode($transObj);
