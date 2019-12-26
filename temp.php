a940ab103e8da1fa3b6b43fb018a96c5919d5e84
Baran Khan <selfcoordinator@gmail.com>
2019-12-26 10:25:12 +0500

    push notificiation received

--- temp.php
+++ temp.php
@@ -20,8 +20,8 @@ $notification['title']='Ride Alert';
 $notification['body']='Passenger is waiting for you.';
 $notification['sound']="ride_alert.mp3";
 $notification['channel_id']="121212";
-$payload['do']="ride_alert";
-$payload['key']="ride_alert";
+$payload['do']="teasing";
+$payload['key']="teasing";
 $payload['msg']="You have a new ride";
 $payload['lat']="30.222079903772556";
 $payload['lng']="71.47192474454641";
@@ -31,9 +31,9 @@ $notification['click_action']='com.barankhan.driver.ride_alert_activity';
 
 
 
-$payload['do']="ride_alert";
+$payload['do']="teasing";
 $payload['msg']="You have a new ride";
-$payload['key']="ride_alert";
+$payload['key']="teasing";
 $payload['lat']="30.222079903772556";
 $payload['lng']="71.47192474454641";
 $payload['ride_id']="2";
@@ -44,16 +44,19 @@ $payload['ride_id']="2";
 
 
 
-$token = "cS5xp-yVVDg:APA91bFX7Mxh6fr_oPf-tf6PFry6Uaac2uvw1skWahSQhVFLq2hzaLOefycesjxmjeWo2SLJ--O9AK7VT7KqjeHk_gieDMvZMaBT1B3VsxRQtI-uulbkVADkuB_ouecI94bAfmcF-Pd0";
-$_token="ecBOb_Cj7P8:APA91bEVQEI1iXNesk3X6xU6_qT_l7uy6lK_pNhJs9S_u3TKuaNjdxU0Btte6nS6E_LG6Jv__ZtM5MZpZC94LDJ5Sfb0Q8ZQeFFGY-ewMOx3aqmgiaDU-_XuGUJK70ZDvyoDPkvh0XF-";
-$token = "cS5xp-yVVDg:APA91bFX7Mxh6fr_oPf-tf6PFry6Uaac2uvw1skWahSQhVFLq2hzaLOefycesjxmjeWo2SLJ--O9AK7VT7KqjeHk_gieDMvZMaBT1B3VsxRQtI-uulbkVADkuB_ouecI94bAfmcF-Pd0";
+$token_ = "cS5xp-yVVDg:APA91bFX7Mxh6fr_oPf-tf6PFry6Uaac2uvw1skWahSQhVFLq2hzaLOefycesjxmjeWo2SLJ--O9AK7VT7KqjeHk_gieDMvZMaBT1B3VsxRQtI-uulbkVADkuB_ouecI94bAfmcF-Pd0";
+$token="ecBOb_Cj7P8:APA91bEVQEI1iXNesk3X6xU6_qT_l7uy6lK_pNhJs9S_u3TKuaNjdxU0Btte6nS6E_LG6Jv__ZtM5MZpZC94LDJ5Sfb0Q8ZQeFFGY-ewMOx3aqmgiaDU-_XuGUJK70ZDvyoDPkvh0XF-";
+$token_ = "cS5xp-yVVDg:APA91bFX7Mxh6fr_oPf-tf6PFry6Uaac2uvw1skWahSQhVFLq2hzaLOefycesjxmjeWo2SLJ--O9AK7VT7KqjeHk_gieDMvZMaBT1B3VsxRQtI-uulbkVADkuB_ouecI94bAfmcF-Pd0";
+$token_ = "fDqB3wnA1F8:APA91bGmzxjORhVk8tZdF0f0o7tXq_GK1FlFlRSaPaQvZXqZax-rTKZha9KWwgWEJuPxph79BSZP74leD9E_Mi-P-WBVuWvNQ2_7OeSSoCZ_EhC_lPu1xSdNQdmF5MGlBH2JNagJ8dXP";
+$token_ = "ecBOb_Cj7P8:APA91bEVQEI1iXNesk3X6xU6_qT_l7uy6lK_pNhJs9S_u3TKuaNjdxU0Btte6nS6E_LG6Jv__ZtM5MZpZC94LDJ5Sfb0Q8ZQeFFGY-ewMOx3aqmgiaDU-_XuGUJK70ZDvyoDPkvh0XF-";
+$token = "esk94lIo4Gc:APA91bHfHchHIv492-4hczbhVohIqLpgmNWT5GuQEasdmS3dnhuaXOR3hVe-5JIk0FAsDyl0oD8qX_ydEfJxc2gMgRaFtXhcJf9Xv2ZpF2rexPe8mLwtsyRapGglLaza8kyX2bZukaE-";
 //$fbaseLogObj->setFirebaseKey($token);
 //$fbaseLogObj->insert();
 
 
 
 
-$fabseRes = $fbaseObj->sendPayloadOnly(1,$token,$payload,null);
+$fabseRes = $fbaseObj->sendPayloadOnly(1,$token,$payload,null,'high');
 
 
 var_dump($fabseRes);
