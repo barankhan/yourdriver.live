e1c3ef1de3f0eb71440376ceabaf07d64e39cccd
Baran Khan <selfcoordinator@gmail.com>
2019-12-26 11:55:08 +0500

    ride accepted

--- rides/api/accept_ride.php
+++ rides/api/accept_ride.php
@@ -24,8 +24,8 @@ if($response=='driver_assigned'){
     $passengerObj->getUserWithId($rideObj->getPassengerId());
 
     $fbaseObj = new firebaseNotification();
-
-    $payload['msg']="Driver is coming at your pickup location.";
+    
+    $payload['message']="Driver is coming at your pickup location.";
     $payload['key']="p_ride_accepted";
     $payload['driver']=json_encode($driverObj);
     $payload['ride']=json_encode($rideObj);
