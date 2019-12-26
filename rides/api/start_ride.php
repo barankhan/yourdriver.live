475f7243b243a71663f2741754e66c9aca099df7
Baran Khan <selfcoordinator@gmail.com>
2019-12-26 16:20:13 +0500

    fix

--- rides/api/start_ride.php
+++ rides/api/start_ride.php
@@ -13,27 +13,33 @@ $driverObj->getUserWithMobile($mobile);
 $rideObj = new ride();
 $rideObj->setId($_REQUEST['ride_id']);
 $rideObj->findRideWithId();
-$rideObj->setIsRideStarted(1);
-$rideObj->setRideStartedAt(date("Y-m-d H:i:s"));
-$rideObj->update();
-$rideObj->setResponse("ride_started");
-$rideObj->setMessage("Your Ride has been started! Enjoy the journey");
 
-$passengerObj  = new User();
-$passengerObj->getUserWithId($rideObj->getPassengerId());
+if($rideObj->getIsRideStarted()==0) {
+    $rideObj->setIsRideStarted(1);
+    $rideObj->setRideStartedAt(date("Y-m-d H:i:s"));
+    $rideObj->update();
+    $rideObj->setResponse("ride_started");
+    $rideObj->setMessage("Your Ride has been started! Enjoy the journey");
 
-
-$payload['message']="Your Ride has been started! Enjoy your journey.";
-$payload['key']="p_ride_started";
-$payload['ride']=json_encode($rideObj);
+    $passengerObj = new User();
+    $passengerObj->getUserWithId($rideObj->getPassengerId());
 
 
-$fbaseObj = new firebaseNotification();
+    $payload['message'] = "Your Ride has been started! Enjoy your journey.";
+    $payload['key'] = "p_ride_started";
+    $payload['ride'] = json_encode($rideObj);
 
 
-$token = $passengerObj->getFirebaseToken();
-$fabseRes = $fbaseObj->sendPayloadOnly($lr->getId(),$token,$payload,$notification,'normal');
+    $fbaseObj = new firebaseNotification();
 
+
+    $token = $passengerObj->getFirebaseToken();
+    $fabseRes = $fbaseObj->sendPayloadOnly($lr->getId(), $token, $payload, $notification, 'normal');
+}else{
+    $rideObj->setResponse("ride_started_earlier");
+    $rideObj->setMessage("you already have started the ride");
+
+}
 
 $var = json_encode($rideObj);
 $lr->setResponseBody($var);
