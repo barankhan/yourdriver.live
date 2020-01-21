f65747d70ebbe8aecf081b6612ace99a8abcc090
Baran Khan <selfcoordinator@gmail.com>
2020-01-21 08:45:18 +0500

    support ticket history

---
+++ support_tickets/api/get_support_ticket_messages.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baran
+ * Date: 1/21/20
+ * Time: 8:41 AM
+ */
+
+require_once __DIR__."/../../vendor/autoload.php";
+
+$user_id= $_REQUEST['user_id'];
+$lr = new LogRequest();
+$lr->setRequestUri($_SERVER['REQUEST_URI']);
+$lr->setRequestBody(json_encode($_REQUEST));
+$lr->setRequestHeader(json_encode($_SERVER));
+$lr->setMobileNumber($user_id);
+$lr->insertLog();
+
+
+$supportTicketMessagesObj = new SupportTicketHistory();
+$supportTicketMessagesObj->setSupportTicketId($_REQUEST['ticket_id']);
+$ticket_histories = $supportTicketMessagesObj->getSupportTicketHistory();
+$res = array();
+foreach($ticket_histories as $history){
+    $obj = new SupportTicketHistory();
+    $obj->setAllFields($history);
+    $res[]=$obj;
+}
+
+
+echo json_encode($res);
+
