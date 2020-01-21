e65ee3074a7cd09dc1640df79879afcdda4ad3bd
Baran Khan <selfcoordinator@gmail.com>
2020-01-21 09:11:51 +0500

    update support ticket

---
+++ support_tickets/api/insert_ticket_message.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baran
+ * Date: 1/21/20
+ * Time: 9:09 AM
+ */
+
+require_once __DIR__."/../../vendor/autoload.php";
+
+$user_id= $_REQUEST['user_id'];
+$lr = new LogRequest();
+$lr->setRequestUri($_SERVER['REQUEST_URI']);
+$lr->setRequestBody(json_encode($_REQUEST));
+$lr->setRequestHeader(json_encode($_SERVER));
+$lr->setMobileNumber($user_id);
+$lr->insertLog();
+
+
+$supportTicketMessagesObj = new SupportTicketHistory();
+$supportTicketMessagesObj->setSupportTicketId($_REQUEST['ticket_id']);
+$supportTicketMessagesObj->setUserId('user_id');
+$supportTicketMessagesObj->setMessage($_REQUEST['message']);
+$supportTicketMessagesObj->insert();
+
+echo json_encode(array("message"=>"Your ticket updated successfully!","response"=>"Ok"));
+
+
+
+
+
