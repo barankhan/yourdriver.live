316ca62fff2f99e3413d2b3b5099da3ca6fbea6c
Baran Khan <selfcoordinator@gmail.com>
2020-07-15 15:58:30 +0500

    fix

--- cron/populate_sms_queue.php
+++ cron/populate_sms_queue.php
@@ -13,7 +13,7 @@ require_once __DIR__."/../vendor/autoload.php";
 
 
 $userObj = new User();
-$drivers = $userObj->getAllDrivers(" and vehicle_type<>'Auto' and is_driver_online=0",1,500);
+$drivers = $userObj->getAllDrivers(" and city='Bahawalpur' ",1,500);
 
 
 foreach ($drivers as $driver){
@@ -21,7 +21,7 @@ foreach ($drivers as $driver){
     $newUserObj->setAllFields($driver);
     $smsQueueObj = new SMSQueue();
     $smsQueueObj->setNumber($newUserObj->getMobile());
-    $smsQueueObj->setMessage("Dear ".$newUserObj->getName()." apni location her waqt update rakhnay ky liye Application ki setting karin: https://yourdriver.live/bo/ shukriya. ");
+    $smsQueueObj->setMessage("Dear ".$newUserObj->getName()."آپ سےگزارش ہےبراۓمہربانی آنلائن رہیں.رائیڈمس ہورہی ہیں.انشاللہ جلد سواریاں زیادہ ملنا شروع ہو جائیں گی.");
     $smsQueueObj->setSendBy(4);
     $smsQueueObj->insert();
 }
