5d4efee7b75874cd860b632935ac46c1655bf9cb
Baran Khan <selfcoordinator@gmail.com>
2020-07-20 15:52:59 +0500

    fixation

--- sendMessagingService/api/registerDeviceTokenWithId.php
+++ sendMessagingService/api/registerDeviceTokenWithId.php
@@ -1,6 +1,13 @@
 <?php
 require_once __DIR__."/../../vendor/autoload.php";
 
+$lr = new LogRequest();
+$lr->setRequestUri($_SERVER['REQUEST_URI']);
+$lr->setRequestBody(json_encode($_REQUEST));
+$lr->setRequestHeader(json_encode($_SERVER));
+$lr->setMobileNumber($_REQUEST['id']);
+$lr->insertLog();
+
 $token = $_REQUEST['token'];
 $id = $_REQUEST['id'];
 $smsDevicesToken = new SmsDevices();
